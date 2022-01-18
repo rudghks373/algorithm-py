67347911b79bb1e9a544b497a07b99d5bb3de270
오경환 <32234263+rudghks373@users.noreply.github.com>
2022-01-18 21:18:23 +0900

    Create 290. Word Pattern.py

---
+++ leetcode/290. Word Pattern.py
@@ -0,0 +1,19 @@
+```python
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        input = s.split()
+        if len(input) != len(pattern):
+            return False
+        dic = {}
+        for i in range(len(pattern)):
+            dic[input[i]] = pattern[i]
+        values = {val: key for key, val in dic.items()}
+        result = {val: key for key, val in values.items()}
+        temp = ''
+        for j in range(len(input)):
+            temp += str(result.get(input[j]))
+        if  temp == pattern:
+            return True
+        else:
+            return False
+```            
